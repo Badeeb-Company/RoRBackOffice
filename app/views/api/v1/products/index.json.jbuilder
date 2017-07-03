a2d00bed0e92962713c40851350b1e38b44f140a
meldeeb <mohammad.edeeb@gmail.com>
2017-07-04 00:42:15 +0200

    Add product name and description

--- app/views/api/v1/products/index.json.jbuilder
+++ app/views/api/v1/products/index.json.jbuilder
@@ -1,4 +1,6 @@
 json.products @products do |product|
 	json.id product.id
 	json.url product.photo_url
+	json.name 'product name'
+	json.description 'product description'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703220035) do
+ActiveRecord::Schema.define(version: 20170703223926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string   "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.string   "description"
   end
 
   create_table "promotion_photos", force: :cascade do |t|
