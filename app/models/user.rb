97d1f84bb490816f31514685d3a1498046646f67
meldeeb <mohammad.edeeb@gmail.com>
2017-07-17 03:18:54 +0200

    Add devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # :registerable, recoverable, :rememberable, :trackable, :registerable
   devise :database_authenticatable, :validatable
+
+  def email_required?
+  	false
+  end
+
+  def email_changed?
+  	false
+  end
 end
