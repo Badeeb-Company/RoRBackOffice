4822895b3a968b80d55f1fe39975d0f9111a46f1
meldeeb <mohammad.edeeb@gmail.com>
2017-10-07 20:41:28 +0200

    Change firebase to production

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 PN_TOPIC_KEKY = 'waritex'
 
 DYNAMIC_LINK_SHORTEN_URL = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks'
-DYNAMIC_LINK_DOMAIN = 'https://me5zd.app.goo.gl/'
+DYNAMIC_LINK_DOMAIN = 'https://r54fw.app.goo.gl/'
 DYNAMIC_LINK_WEBSITE = 'http://waritex.com'
 
 ANDROID_PACKAGE_NAME = 'com.badeeb.waritex'
